3fde941596b54e9cd0fb54ee807af2180f028a00
Matt Currier <fingerskier@gmail.com>
2025-06-10 18:11:26 -0500

    Add parallel state machine example (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import One from '@/com/One'
 import Two from '@/com/Two'
 import Three from '@/com/Three'
 import Controls from '@/com/Controls'
+import Alpha from '@/com/Alpha'
+import Beta from '@/com/Beta'
+import Controls2 from '@/com/Controls2'
 
 
 export default function App() {
@@ -20,6 +23,18 @@ export default function App() {
 
       <State name="three" transition={['one']}>
         <Three />
+      </State>
+    </StateMachine>
+
+    <StateMachine initial='alpha' name='aux'>
+      <Controls2 />
+
+      <State name="alpha" transition={['beta']}>
+        <Alpha />
+      </State>
+
+      <State name="beta" transition={['alpha']}>
+        <Beta />
       </State>
     </StateMachine>
   </>
---
+++ src/com/Controls2.tsx
@@ -0,0 +1,8 @@
+import {StateButton} from '@/StateMachine'
+
+export default function Controls2() {
+    return <div>
+        <StateButton to='alpha'>Alpha</StateButton>
+        <StateButton to='beta'>Beta</StateButton>
+    </div>
+}
