26fc5f0dc57f5b37488860ca697f695ca5b54efd
Matt Currier <fingerskier@gmail.com>
2025-07-09 09:28:11 -0500

    feat(StateMachine): add optional className wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,9 @@ export default function App() {
 
     <ExternalButton className='badge' to='one' machine='app'>Start machine #1</ExternalButton>
 
-    <StateMachine name='app'>
-      <div className='state machine'>
-        <h1>First State-Machine</h1>
-        <ExternalButton className='badge' 
+    <StateMachine name='app' className='state machine'>
+      <h1>First State-Machine</h1>
+        <ExternalButton className='badge'
           to='alpha' machine='aux'
           data={{alpha: 0, beta: 'nada', gamma: 0, delta: 0}}
         >Start machine #2</ExternalButton>
@@ -34,13 +33,9 @@ export default function App() {
         <State name={M1.ST[2]}>
           <Three />
         </State>
-      </div>
     </StateMachine>
 
-    <div className='state machine'>
-      <StateMachine name='aux' initial='alpha'>
+    <StateMachine name='aux' initial='alpha' className='state machine'>
         <SecondMachine />
-      </StateMachine>
-    </div>
-  </>
-}
+    </StateMachine>
+  </>}
